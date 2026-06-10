823eaa18b076307efb1c5ce879849c9ddc2fe633
jjcalhoun <114455306+jjcalhoun@users.noreply.github.com>
2026-06-10 01:58:47 -0400

    Update config.js

--- config.js
+++ config.js
@@ -13,7 +13,7 @@ const SUPABASE_ANON_KEY = "sb_publishable_r7WGyawAJPZb_dU-kqeuQA_grUJV7BP";
 
 // A private word only you know. Anyone visiting admin.html must type this
 // to view responses. Change it to something only you would guess.
-const ADMIN_PASSPHRASE = "change-this-to-something-secret";
+const ADMIN_PASSPHRASE = "D5a1t2a3!";
 
 if (typeof module !== "undefined" && module.exports) {
   module.exports = { SUPABASE_URL, SUPABASE_ANON_KEY, ADMIN_PASSPHRASE };
